aaab6e9d2ccfa34723be1bd6fa74d1ca69d69ccf
Michael Viernes <92641515+MichaelViernes271@users.noreply.github.com>
2021-11-15 20:03:21 +0800

    Laying funcitons to assemble the code.
    commit 7c34d6eda530b0910d9a38a869c2c70aac9c5f50 (HEAD -> main)
    Author: MichaelViernes271 <michaeleviernes@iskolarngbayan.pup.edu.ph>
    Date:   Mon Nov 15 20:02:07 2021 +0800
    
                modified:   pup-grading-system.py
    
         Laying the functions for implementation of the code planning how to start.
         Cannot be run.

--- pup-grading-system.py
+++ pup-grading-system.py
@@ -5,7 +5,27 @@ College: BSCOE 1-1
 Submitted to: Mr. Danilo Madrigalejos
 """
 
+import math as m # Might be useful at some point.
 
+def inputGrade(): # Asks number of grade.
+
+
+def equateGrade(): # Equivalent of grade in card.
+
+
+def descGrade(): # Determines the grade starting from withrawal to excellent description.
+
+
+def printGrade(): # Prints the grade.
+
+
+def main(): # main().
+    inputGrade()
+    equateGrade()
+    descGrade()
+    printGrade()
+    
+"""
 while True: # My template for usual main().
     main()
     quit = input("Quit (y/n): ")
@@ -15,3 +35,4 @@ while True: # My template for usual main().
     if (quit == 'y' or quit == 0):
         print("Closing...\n")    
         break
+"""
