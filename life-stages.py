a844410c50b4e7d39c9dae7be831325bc5344aea
Michael Viernes <92641515+MichaelViernes271@users.noreply.github.com>
2021-11-15 10:44:48 +0800

    Progress of code but untested.
    commit f7e9b5706a907035691e60e08485dd6c337cc601 (HEAD -> main)
    Author: MichaelViernes271 <michaeleviernes@iskolarngbayan.pup.edu.ph>
    Date:   Mon Nov 15 10:41:38 2021 +0800
    
         On branch main
         Changes to be committed:
                modified:   life-stages.py
    
         Committed for progress of work done in main branch where:
          1. Codes were typewritten but not tested; &
          2. Saving the code.

--- life-stages.py
+++ life-stages.py
@@ -6,14 +6,42 @@ Submitted to: Mr. Danilo Madrigalejos
 """
 
 # =================NOTE===================
-# PLEASE TRY SEARCHING FOR SWITCH 
-# STATEMENT IN PYTHON IF ANY.
-# ALSO, RESEARCH ABOUT DECOR FUNCS
-# 
+# X = PLEASE TRY SEARCHING FOR SWITCH 
+# X = STATEMENT IN PYTHON IF ANY.
+# X = ALSO, RESEARCH ABOUT DECOR FUNCS.
+# ========================================
 
-def classify_age(age): # Classifies age.
 
-def ask_age(): # Requests age.
+def compareAge(u_age): # Returns the age class.
 
-def main(): # Main Func.
+    try:
+        int kid, teen debut, adult = 12, 17, 18, 19
+        
+        if u_age =< kid:
+            return "kid"
+        elif kid < u_age =< teen:
+            return "teen"
+        elif debut == u_age:
+            return "debut"
+        elif u_name > adult:
+            return "adult"
+        else:
+            return "Not a valid value. Please try again."
+    except InvalidInput:
 
+def printAge(age, category): # Prints result.
+
+
+def askAge(): # Requests age.
+    try:
+        return int(input("What is your age: "))
+    except TypeError as e:
+        e
+
+def main(): # Main().
+    print(
+    """
+       ========================================
+       \t\tLIFE STAGES
+       ========================================
+    """)
