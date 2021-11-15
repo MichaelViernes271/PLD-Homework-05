2ee57ddcd0976499d098b329a4d69bb0ca318a31
Michael Viernes <92641515+MichaelViernes271@users.noreply.github.com>
2021-11-15 19:52:48 +0800

    Removed the unnecessary print debugs.
    commit a9b628f8663bbae13755238c4aa01a8ed82c840f (HEAD -> main)
    Author: MichaelViernes271 <michaeleviernes@iskolarngbayan.pup.edu.ph>
    Date:   Mon Nov 15 19:48:23 2021 +0800
    
        Removed the unnecessary print debugs.

--- min-number.py
+++ min-number.py
@@ -5,44 +5,28 @@ College: BSCOE 1-1
 Submitted to: Mr. Danilo Madrigalejos
 """
 
-# =================NOTE===================
+# =================REMINDER NOTE===================
 # PLEASE TRY SEARCHING FOR SWITCH 
 # STATEMENT IN PYTHON IF ANY.
-# ALSO, RESEARCH ABOUT DECOR FUNCS
-
-
-                                        # ################### #
-                                        #  Efficient sorting  #
-                                        # ################### #
-
-
-
+# ALSO, RESEARCH ABOUT DECOR FUNCS.
 
 def minNum(numlist): # Find the min number among the inputs.
-    # print(str(numlist[0]))
     return numlist[0]
 
 def sortNum(numlist): # Sorts two numbers.
-    # print("sortNum()" + str(numlist))
-    # print("sorting " + str(numlist.sort()))
     sorted_list = list(numlist)
     sorted_list.sort()
-    # print("the sorted list" + str(sorted_list))
     return sorted_list
     
 def askNum(): # Asks a multiple numbers.
     numlist = []
     for i in range(1, 4):
-        # print(i)
         numlist.append((int(input("Type an integer: "))))
-    # print("the list" + str(numlist))
     return numlist.copy()
 def main(): # main().
     u_numlist = []
     u_numlist = list(askNum())
-    # print(f"askNum() has finished its task!\n {u_numlist}")
     u_numlist = sortNum(list(u_numlist))
-    # print(f"sortNum() has finished its task!\n{u_numlist}")
     min_num = minNum(u_numlist)
     print(f"The minimum value among {list(u_numlist)} is {min_num}.\n")
     
